957695d4420c6987c24b501ca2f269970d1104df
Obananob <97780446+Obananob@users.noreply.github.com>
2025-07-23 23:37:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import requests
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
-from huggingface_hub import InferenceClient
 from twilio.twiml.messaging_response import MessagingResponse
 
 load_dotenv()
 
-app = FastAPI(title= "AgroScan – Plant Disease Detector")
+app = FastAPI(title="AgroScan – Plant Disease Detector")
 
-# Load model
-MODEL = tf.keras.models.load_model("model/agroscan_model.keras")
+# Load TFLite model
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # Class labels
 CLASS_NAMES = [
@@ -41,81 +43,49 @@ TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 twilio_client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-# # Hugging Face setup
-# Client = InferenceClient(
-#     provider = "featherless-ai",
-#     api_key = os.getenv("HF_TOKEN")
-# )
-# HF_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-
-
 def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
-
+    image = Image.open(BytesIO(data)).convert("RGB").resize((224, 224))
+    image_array = np.array(image, dtype=np.float32) / 255.0
+    return np.expand_dims(image_array, axis=0)
 
 # Predict endpoint
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
 
-    prediction = MODEL.predict(img_batch)
-    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
-    confidence = np.max(prediction[0])
-    # filter inputs with low confidence
+    interpreter.set_tensor(input_details[0]['index'], image)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    prediction = output_data[0]
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = float(np.max(prediction))
+
     if confidence < 0.7:
         return {
             "class": "Uncertain",
-            "confidence": float(confidence),
-          #  "treatment": "Please Upload a clear Plant Leaf Image"
+            "confidence": confidence
         }
-   # treatment = get_treatment_recommendation(predicted_class)
 
     return {
         "class": predicted_class,
-        "confidence": float(confidence),
-        # "treatment": treatment
+        "confidence": confidence
     }
 
-# # LLM Integration
-# def get_treatment_recommendation(disease: str):
-#     prompt = (
-#         f"Think carefully. A plant has benn diagnosed with the disease:{disease}."
-#     "What is a simple and actionable treatment recommendation?"
-#     "Respond in the same language as the disease name"
-#     )
-#
-#     try:
-#         completion = Client.chat.completions.create(
-#             model=HF_MODEL,
-#             messages=[
-#                 {"role": "user", "content": prompt}
-#             ],
-#             temperature = 0.6,
-#             max_tokens = 200,
-#             timeout= 60
-#         )
-#         return completion.choices[0].message.content.strip()
-#     except Exception as e:
-#         return f"Error from Hugging Face LLM: {str(e)}"
-#
-
 # Whatsapp Webhook
-@app.post("/hook",
-response_class=PlainTextResponse)
+@app.post("/hook", response_class=PlainTextResponse)
 async def whatsapp_hook(request: Request):
-        data = await request.form()
-        user_msg = data.get("Body", "").strip().lower()
+    data = await request.form()
+    user_msg = data.get("Body", "").strip().lower()
 
-        if "hi" in user_msg or "hello" in user_msg:
-            reply = "Welcome to AgroScan! Send me a plant leaf image and I’ll tell you if it’s sick and what to do."
-        else:
-            reply = "Please upload a clear leaf image so I can analyze it."
+    if "hi" in user_msg or "hello" in user_msg:
+        reply = "Welcome to AgroScan! Send me a plant leaf image and I’ll tell you if it’s sick and what to do."
+    else:
+        reply = "Please upload a clear leaf image so I can analyze it."
 
-        response= MessagingResponse()
-        response.message(reply)
-        return PlainTextResponse(str(response))
+    response = MessagingResponse()
+    response.message(reply)
+    return PlainTextResponse(str(response))
 
 @app.get("/")
 def root():
